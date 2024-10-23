f2b80db8383488928f3dbfea26878e00d09f9797
José Gameiro <93797961+zegameiro@users.noreply.github.com>
2024-10-23 16:04:10 +0100

    Esnook 30 map screen (#2)
    * ESNOOK-30 Added google maps api key
    
    * ESNOOK-30 Added some libraries to allow the use of Google Maps
    
    * ESNOOK-30 Created google-services.json obtain through FireBase
    
    * ESNOOK-30 Changed App name, added permissions to android manifest and added some images
    
    * ESNOOK-30 Added some small fixes to the theme and create map manager
    
    * ESNOOK-30 Created map screen and changes in the main activity

--- app/src/main/java/com/es_g05/nook_app/MainActivity.kt
+++ app/src/main/java/com/es_g05/nook_app/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.es_g05.nook_app
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,21 +15,44 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.es_g05.nook_app.ui.screens.MapScreen
 import com.es_g05.nook_app.ui.theme.Nook_appTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Register the permission launcher before calling setContent()
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                recreate()
+            }
+        }
         enableEdgeToEdge()
         setContent {
             Nook_appTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "from Nook",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MapScreen()
             }
+        }
+    }
+
+    // Request permission function
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED) {
+            // Permission already granted, proceed with showing the map
+            recreate() // Or use a proper method to trigger recomposition
+        } else {
+            // Request the permission
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
 }
