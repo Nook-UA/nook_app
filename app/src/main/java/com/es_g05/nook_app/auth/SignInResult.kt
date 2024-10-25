f367e0f65d99c737cda9f06847763eef64b7de46
zegameiro <jose.mcgameiro@ua.pt>
2024-10-25 20:32:35 +0100

    ESNOOK-47 Added Sign In result, state and view model

---
+++ app/src/main/java/com/es_g05/nook_app/auth/SignInResult.kt
@@ -0,0 +1,12 @@
+package com.es_g05.nook_app.auth
+
+data class SignInResult(
+    val data: UserData?,
+    val errorMessage: String?
+)
+
+data class UserData(
+    val userId: String,
+    val userName: String?,
+    val profilePictureUrl: String?
+)
