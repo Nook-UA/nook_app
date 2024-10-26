40e6fd56e398388c07bed5a315b6c8696882f655
zegameiro <jose.mcgameiro@ua.pt>
2024-10-26 17:44:29 +0100

    ESNOOK-47 Changed sign in screen according to mockup

--- app/src/main/java/com/es_g05/nook_app/ui/screens/SignInScreen.kt
+++ app/src/main/java/com/es_g05/nook_app/ui/screens/SignInScreen.kt
@@ -1,18 +1,36 @@
 package com.es_g05.nook_app.ui.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.es_g05.nook_app.R
 import com.es_g05.nook_app.auth.SignInState
+import com.es_g05.nook_app.ui.components.GoogleSignInButton
 
 @Composable
 fun SignInScreen(
@@ -20,6 +38,7 @@ fun SignInScreen(
     onSignInClick: () -> Unit
 ) {
     val context = LocalContext.current
+
     LaunchedEffect(key1 = state.signInError) {
         state.signInError?.let { error ->
             Toast.makeText(
@@ -30,14 +49,115 @@ fun SignInScreen(
         }
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Button(onClick = onSignInClick) {
-            Text("Sign In")
+    Box(modifier = Modifier.fillMaxSize().padding(top = 32.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.login_screen),
+            contentDescription = "Login Background",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            MainTitle()
+
+            SubTitle()
+
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                GoogleSignInButton(onSignInClick = onSignInClick)
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                ClickableText(
+                    text = AnnotatedString("Continue without logging in"),
+                    onClick = { },
+                    modifier = Modifier.padding(top = 8.dp),
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        textAlign = TextAlign.Center,
+                        textDecoration = TextDecoration.Underline
+                    )
+                )
+            }
         }
+    }
+}
+
+@Composable
+fun MainTitle() {
+    Column(
+        modifier = Modifier.width(300.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Get yourself a",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            textAlign = TextAlign.Center,
+        )
+        Row {
+            Text(
+                text = "place to",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.padding(horizontal = 5.dp))
+
+            Text(
+                text = "park",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFFC55002),
+                textAlign = TextAlign.Center
+            )
+        }
+        Text(
+            text = "your car",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFC55002),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun SubTitle() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Start today and save",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "time by knowing where",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "to park",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
     }
 }
