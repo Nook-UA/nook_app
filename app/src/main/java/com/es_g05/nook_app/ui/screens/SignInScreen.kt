d65d1e347a8668a675a7fbd4edce326b33524867
zegameiro <jose.mcgameiro@ua.pt>
2024-11-18 10:37:56 +0000

    ESNOOK-47 Removed continue with sign in option

--- app/src/main/java/com/es_g05/nook_app/ui/screens/SignInScreen.kt
+++ app/src/main/java/com/es_g05/nook_app/ui/screens/SignInScreen.kt
@@ -8,10 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,11 +19,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.es_g05.nook_app.R
@@ -71,19 +66,6 @@ fun SignInScreen(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 GoogleSignInButton(onSignInClick = onSignInClick)
-
-                Spacer(modifier = Modifier.height(8.dp))
-
-                ClickableText(
-                    text = AnnotatedString("Continue without logging in"),
-                    onClick = { },
-                    modifier = Modifier.padding(top = 8.dp),
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        textAlign = TextAlign.Center,
-                        textDecoration = TextDecoration.Underline
-                    )
-                )
             }
         }
     }
