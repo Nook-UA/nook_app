7b1915f7acc9793076e0dd71791500d6c8209a0e
zegameiro <jose.mcgameiro@ua.pt>
2024-11-24 18:29:05 +0000

    Created a component for the park marker

--- app/src/main/java/com/es_g05/nook_app/ui/components/ParkMarkerComponent.kt
+++ app/src/main/java/com/es_g05/nook_app/ui/components/ParkMarkerComponent.kt
@@ -1,2 +1,25 @@
 package com.es_g05.nook_app.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.es_g05.nook_app.R
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.MarkerComposable
+import com.google.maps.android.compose.MarkerState
+
+@Composable
+fun ParkMarkerComposable(position: LatLng) {
+    MarkerComposable(
+        state = MarkerState(position = position)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.location_marker),
+            contentDescription = "",
+            modifier = Modifier.size(50.dp)
+        )
+    }
+}
