07d4b6e30c7752ebf2dea86ec375d5c275e71c5b
zegameiro <jose.mcgameiro@ua.pt>
2024-11-24 18:30:02 +0000

    ESNOOK-74 Created Component for parking information

--- app/src/main/java/com/es_g05/nook_app/ui/components/ParkInformation.kt
+++ app/src/main/java/com/es_g05/nook_app/ui/components/ParkInformation.kt
@@ -1,2 +1,12 @@
 package com.es_g05.nook_app.ui.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ParkInformation(
+    name: String,
+    parking_spots: String
+) {
+
+}
