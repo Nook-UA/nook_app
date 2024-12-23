ceab738a194b22afc6b3f35d910b9404a6a1d4b1
zegameiro <jose.mcgameiro@ua.pt>
2024-12-23 15:17:00 +0000

    ESNOOK-74 Add app container to navigation and main screen, and function to fetch nearby parks in the map view model

--- app/src/main/java/com/es_g05/nook_app/view_models/MapViewModel.kt
+++ app/src/main/java/com/es_g05/nook_app/view_models/MapViewModel.kt
@@ -3,15 +3,30 @@ package com.es_g05.nook_app.view_models
 import android.location.Location
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.es_g05.nook_app.repositories.NookParksRepository
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
-import com.google.maps.android.compose.MapUiSettings
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class MapViewModel(): ViewModel() {
+class MapViewModelFactory(
+    private val nookParksRepository: NookParksRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(MapViewModel::class.java)) {
+            return MapViewModel(nookParksRepository = nookParksRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class MapViewModel(
+    private val nookParksRepository: NookParksRepository
+): ViewModel() {
+
     private var _location = MutableStateFlow<Location>(
         Location("DEFAULT_LOCATION").apply {
             latitude = 40.638076
@@ -20,22 +35,32 @@ class MapViewModel(): ViewModel() {
     )
 
     val location: StateFlow<Location> = _location
-    val uiSettings = mutableStateOf(MapUiSettings())
     val properties = mutableStateOf(MapProperties(mapType = MapType.NORMAL))
 
     init {
-        viewModelScope.launch {
-            val userLocation = getCoordinates()
-            _location.emit(userLocation)
-        }
-
+        getNearbyParks()
     }
 
     fun updateLocation(location: Location) {
-        this._location = MutableStateFlow(location)
+        _location.value = location
+        getNearbyParks()
     }
 
     fun getCoordinates(): Location {
         return _location.value
+    }
+
+    fun getNearbyParks() {
+        viewModelScope.launch {
+            try {
+                val parks = nookParksRepository.getNearbyParks(
+                    _location.value.latitude,
+                    _location.value.longitude
+                )
+                println("Nearby Parks: $parks")
+            } catch (e: Exception) {
+                println("Error fetching parks: ${e.message}")
+            }
+        }
     }
 }
